bccfc4db985ba34ad5512c8175cfb757a567fba2
Matthew Robbins <matthew.r.robbins@gmail.com>
2017-12-10 13:15:58 -0500

    Initial commit for Spring Boot 1.5.8.RELEASE and Spring Cloud Edgware.RELEASE

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Dependency Checker
+
+Build the dependency tree for Spring Boot and Spring Cloud using Maven
+
+```
+mvn dependency:tree -DoutputFile=deps.out
+```
---
+++ src/main/java/com/github/mrrobbins/springdependencieschecker/SpringDependenciesCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.github.mrrobbins.springdependencieschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependenciesCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDependenciesCheckerApplication.class, args);
+	}
+}
